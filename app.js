85085d1c957d3b5b9e1733929297ef53b3616a2b
Akshat Shah <akshatshah@nichols-ve703-01431.apn.wlan.private.upenn.edu>
2021-11-21 13:32:54 -0500

    implemented ticket fetching w/ http request

--- app.js
+++ app.js
@@ -6,6 +6,11 @@
    var app = express();
    app.use(express.urlencoded({extended: true}));
    app.use(cookieParser());
+   app.use(express.json());
+
+
+   //homepage route -- linked to homepage in routes.js
+   app.get("/",routes.get_homepage);
    
    app.listen(8080);
    console.log('Server running on port 8080. Now open http://localhost:8080/ in your browser!');
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,35 @@
 var express = require('express');
+var https = require("https");
 var router = express.Router();
 
+//info to do GET request
+var options = {
+  hostname: domain,
+  path: "/api/v2/tickets.json",
+  method: "GET",
+  headers: {
+    Authorization: 'Basic ' + Buffer.from(username+":"+password).toString("base64")
+ } 
+}
+
+
+var ticket_request = https.get(options, 
+                    function (res) {
+                        //allows for return data to be readable
+                        res.setEncoding('utf8');
+                        res.on('data', function (body) {
+                        console.log(body);
+                      });
+                    });
+
+
 /* GET home page. */
-var homepage = router.get('/', function(req, res, next) {
-  res.render('home.ejs');
+var homepage = router.get('', function(req, res, next) {
+  ticket_request;
 });
 
-module.exports = router;
+var routes = {
+  get_homepage : homepage
+}
+
+module.exports = routes;
