580f61b246f85e2fe78a797c0ac42cb97ba43ecf
Akshat Shah <akshatshah@nichols-ve703-01431.apn.wlan.private.upenn.edu>
2021-11-21 23:15:15 -0500

    tickets are now displayed on the homepage

--- routes/routes.js
+++ routes/routes.js
@@ -1,32 +1,54 @@
+const { render } = require('ejs');
+const { request, json } = require('express');
 var express = require('express');
-var https = require("https");
-var router = express.Router();
+var axios = require('axios');
 
-//info to do GET request
-var options = {
-  hostname: domain,
-  path: "/api/v2/tickets.json",
-  method: "GET",
-  headers: {
-    Authorization: 'Basic ' + Buffer.from(username+":"+password).toString("base64")
- } 
+
+
+//request to get tickets body
+var request_body = function(callback){
+  axios.get(domain,{
+    auth: {
+      username: username,
+      password: password,
+    },
+  }).then(callback);
+}
+
+//seperates array in size of 25 for pages
+var paginate = function (arr, size) {
+  return arr.reduce((acc, val, i) => {
+    let idx = Math.floor(i / size);
+    let page = acc[idx] || (acc[idx] = []);
+    page.push(val);
+    return acc;
+  }, []);
 }
 
 
-var ticket_request = https.get(options, 
-                    function (res) {
-                        //allows for return data to be readable
-                        res.setEncoding('utf8');
-                        res.on('data', function (body) {
-                        console.log(body);
-                      });
-                    });
-
-
 /* GET home page. */
-var homepage = router.get('', function(req, res, next) {
-  ticket_request;
-});
+var homepage = function(req, res){
+  request_body((req)=>{
+    var tickets = req.data.tickets;
+    var ticketsArr = [];
+    //we only want the neccesary information and compile it into a dict.
+    tickets.forEach(element => {
+      console.log(element);
+      var ticket = {
+        subject : element.subject,
+        opened_by : element.assignee_id,
+        created_at : element.created_at,
+        description : element.description,
+        tags : element.tags
+      }
+      ticketsArr.push(ticket);
+    })
+      //we send the page, the total number of tickets, and the current page the user is on.
+      res.render('home.ejs', {ticketsArr : paginate(ticketsArr, 25), count : req.data.count, pageNum : 0})
+  })
+};
+
+
 
 var routes = {
   get_homepage : homepage
