a74521ddaac00477ee1c2b306b792782b9501114
Akshat Shah <akshatshah@nichols-ve703-01431.apn.wlan.private.upenn.edu>
2021-11-22 12:36:28 -0500

    fixed bug where each ticket gets the same popup

--- public/js/ticketpopup.js
+++ public/js/ticketpopup.js
@@ -1,24 +1,25 @@
-$(document).ready(() => {
-    var tileArr = document.getElementsByClassName("tile");
-    var modalArr = document.getElementsByClassName("modal");
-    var backgroundModalsArr = document.getElementsByClassName('modal-background');
-    var modelCloseArr = document.getElementsByClassName('modal-close');
+function addModal(element){
+    //this helps get the element that called this function
+    var intID = parseInt(element.id);
+    //then we use this to obtain appropriate modal 
+    var model = document.getElementById('modal ' + intID);
+    var html = document.getElementsByTagName('html')[0];
+    var modelBackground = document.getElementById('background ' + intID);
+    var modelCloseButton = document.getElementById('modal-close ' + intID);
 
-    //we take each modal and each tile and assign then to ticket desc.
-    for(var i = 0; i < modalArr.length; i++){
-            var currClasses = modalArr[i].classList;
+    //this opens the model when clicked and stops scrolling
+    model.classList.add('is-active');
+    html.classList.add('is-clipped');
 
-            tileArr[i].addEventListener('click', function(){
-                currClasses.toggle('is-active');
-                $('html').toggleClass('is-clipped');
-            })
-            backgroundModalsArr[i].addEventListener('click', function(){
-                currClasses.toggle('is-active');
-                $('html').toggleClass('is-clipped');
-            })
-            modelCloseArr[i].addEventListener('click', function(){
-                currClasses.toggle('is-active');
-                $('html').toggleClass('is-clipped');
-            })
-    }
- });
+    //if the background or x is clicked, we resume to normal.
+    modelBackground.addEventListener('click', function(){
+        html.classList.remove('is-clipped');
+        model.classList.remove('is-active');
+    })
+
+    modelCloseButton.addEventListener('click', function(){
+        html.classList.remove('is-clipped');
+        model.classList.remove('is-active');
+    })
+
+}
