9440143cad672117139cb712e4bd3ed024ae3911
Akshat Shah <akshatshah@Akshats-MacBook-Pro.local>
2021-11-25 13:24:36 -0600

    fixed query to only query once when / called

--- app.js
+++ app.js
@@ -1,31 +1,40 @@
-   var express = require('express');
-   var routes = require('./routes/routes.js');
-   var cookieParser = require('cookie-parser');
-   
-   
-   var app = express();
-   app.use(express.urlencoded({extended: true}));
-   app.use(cookieParser());
-   app.use(express.json());
-   app.use(express.static('public'));
+var express = require('express');
+var routes = require('./routes/routes.js');
+var cookieParser = require('cookie-parser');
+var app = express();
 
-   //homepage route -- linked to homepage in routes.js
-  app.get("/",routes.get_homepage);
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.static('public'));
 
-    //Handling 404
-    app.use(function (req, res, next) {
-      var message = "Sorry, the page you requested cannot be found."
-                     + " Double check to ensure you tried accessing the correct page." 
-                     + " In the meantime, I will send you back to the homepage.";
-      res.status(404).redirect("/?message="+encodeURIComponent(message));
-    })
+//homepage route -- linked to homepage in routes.js
+app.get("/", function(req, res) {
+  if (!req.query.page && !req.query.message){
+    console.log("query for page " + req.query.page + ".");
+    console.log("go to tickets");
+    routes.fetch_tickets(req, res);
+  }else{
+    console.log("Message query: " + req.query.message);
+    console.log("go to pages: " + JSON.stringify(req.query.page) + ".");
+    routes.get_page(req, res);
+  }
+})
 
-  // Handling 500 (Internal Server Error)
-  app.use(function (err, req, res, next) {
-    message = "Hmmm. It seems something went wrong! Try again." 
-                +" If this persists, contact a server admin!"
-    res.status(500).redirect("/?message="+encodeURIComponent(message));
-  })
+// Handling 500 (Internal Server Error)
+app.use(function (err, req, res, next) {
+  message = "Hmmm. It seems something went wrong! Try again."
+    + " If this persists, contact a server admin!"
+  res.status(500).send(message);
+})
 
-  app.listen(8080);
-  console.log('Server running on port 8080. Now open http://localhost:8080/ in your browser!');
+//Handling 404
+app.use(function (req, res, next) {
+  var message = "Sorry, the page you requested cannot be found."
+    + " Double check to ensure you tried accessing the correct page."
+    + " In the meantime, I will send you back to the homepage.";
+  res.status(404).redirect("/?message=" + encodeURIComponent(message));
+})
+
+app.listen(8080);
+console.log('Server running on port 8080. Now open http://localhost:8080/ in your browser!');
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,14 @@
 const { render } = require('ejs');
 const { request, json } = require('express');
-var express = require('express');
 var axios = require('axios');
 const e = require('express');
+var ticketsArr = [];
 
 
 //request to get tickets body
-var request_body = function(callback, error){
-  //if undefined then set to empty
+var request_body = function (callback, error) {
 
-  axios.get(domain,{
+  axios.get(domain, {
     auth: {
       username: username,
       password: password,
@@ -27,54 +26,64 @@ var paginate = function (arr, size) {
   }, []);
 }
 
-var convert_date = function(iso){
+var convert_date = function (iso) {
   var d = new Date(iso);
   d.toLocaleDateString('en-US');
   return d;
 }
 
 
+var get_page = function(req, res){
+  var page = req.query.page;
+  if(ticketsArr.length == 0){
+    res.redirect('/');
+  }else{
+    //we send the page, the total number of tickets, and the current page the user is on.
+    if (page > 0 && (page <= ticketsArr.length)) {
+      res.render('home.ejs', { ticketsArr: ticketsArr[page - 1], pages: ticketsArr.length, pageNum: page, message: req.query.message });
+    } else {
+      var message = "It seems like you tried to enter an invalid query in the URL! You were redirected to the first page.";
+      res.status(404).render('home.ejs', { ticketsArr: ticketsArr[0], pages: ticketsArr.length, pageNum: 1, message: message });
+    }
+  }
+  
+}
+
+
 /* GET home page. */
-var homepage = function(req, pageRes){
-  request_body((res)=>{
-        var ticketsArr = [];
-        var tickets = res.data.tickets;
-        //we only want the neccesary information and compile it into a dict.
-        tickets.forEach(element => {
-          var ticket = {
-            subject : element.subject,
-            opened_by : element.assignee_id,
-            created_at : convert_date(element.created_at),
-            description : element.description,
-            tags : element.tags,
-            priority : element.priority,
-          }
-          ticketsArr.push(ticket);
-        })
-          var page = req.query.page;
-          var pagedArr = paginate(ticketsArr, 25);
-    
-          //we send the page, the total number of tickets, and the current page the user is on.
-          if(page>0 && (page <= pagedArr.length)){
-            
-            pageRes.render('home.ejs', {ticketsArr : pagedArr[page-1], pages : pagedArr.length, pageNum : page, message: req.query.message});
-          }else{
-            if (page == null){
-              pageRes.render('home.ejs', {ticketsArr : pagedArr[0], pages : pagedArr.length, pageNum : 1, message: req.query.message});
-            }else{
-              var message = "It seems like you tried to enter an invalid query in the URL! You were redirected to the first page.";
-              pageRes.redirect("/?message="+encodeURIComponent(message));
-            }
-          }
-    }, (err)=>{
-        //unauthenticated
-        pageRes.status(401).render('error.ejs', {status: err.response.status, message: JSON.stringify(err.response.data)});
+var fetch_tickets = function (req, pageRes) {
+  request_body((res) => {
+    var tickets = res.data.tickets;
+    ticketsArr = []
+    //we only want the neccesary information and compile it into a dict.
+    tickets.forEach(element => {
+      var ticket = {
+        subject: element.subject,
+        opened_by: element.assignee_id,
+        created_at: convert_date(element.created_at),
+        description: element.description,
+        tags: element.tags,
+        priority: element.priority,
+      }
+      ticketsArr.push(ticket);
     })
+    ticketsArr = paginate(ticketsArr, 25);
+    pageRes.redirect("/?page=1");
+  },(function(){
+    if (typeof err.response == 'undefined') {
+      message = "Unable to connect to the ZenDesk API";
+      pageRes.status(400).render('error.ejs', { message: message });
+    } else {
+      //unauthenticated
+      pageRes.status(401).render('error.ejs', { status: err.response.status, message: JSON.stringify(err.response.data) });
+    }
+  }));
 };
 
 
 var routes = {
-  get_homepage : homepage
+  fetch_tickets: fetch_tickets,
+  get_page : get_page
 };
 
 module.exports = routes;
